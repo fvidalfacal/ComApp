6d2c3f45ed52ee0641f0676d123e4ccdcd4c9273
Florian VIDAL FACAL <florianvidalfacal@gmail.com>
2014-11-19 16:47:23 +0100

    * hotfix authentication

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-if (!isset($_SESSION['connect']) || $_SESSION['connect'] = false) {
+if (!isset($_SESSION['connect']) || $_SESSION['connect'] == false) {
     header("Location: login.php");
 }
 ?>
